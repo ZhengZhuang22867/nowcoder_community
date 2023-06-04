2e79ba39417967605846ee2245a0073eae9c79fa
Zheng Zhuang <1525019749@qq.com>
2023-06-04 10:45:36 +0100

    2.01 发送邮件 内容：在qq邮箱的设置中（【账户】一栏）开启smtp服务；导入jar包；在applicationProperties中配置mail属性；在测试中进行测试：使用JavaMailSender和html模板发送邮件。

---
+++ src/test/java/com/nowcoder/community/MailTest.java
@@ -0,0 +1,39 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.Thymeleaf;
+import org.thymeleaf.context.Context;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTest {
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail(){
+        mailClient.sendMail("13583238187@163.com", "Test", "Welcome!");
+    }
+
+    @Test
+    public void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("13583238187@163.com", "HTML", content);
+    }
+}
