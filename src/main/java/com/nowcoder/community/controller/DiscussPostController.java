26b21ee5cea8a8967e71d9436b312e4dfb8ce6f7
Zheng Zhuang <1525019749@qq.com>
2023-06-19 15:58:15 +0100

    3.20 显示评论 思路：   整体流程就是显示主页的复刻，并没有使用新的技术，就当是复习了。评论分成两种：一种是对帖子的评论，还有一种是对评论的评论。这俩种都需要分页处理。 内容：   1.更新CommunityConstant，添加两个实体类型的常量：帖子和评论；   2.创建Comment实体，CommentMapper和CommentService，实现数据访问层和业务层的逻辑；   3.【核心】更新DiscussPostController的getDiscussPost方法，实现表现层的逻辑。不难但是有点绕；   4.更新index.html（就一行）和discuss-detail.html，注意[对象名]Stat.count的使用。

--- src/main/java/com/nowcoder/community/controller/DiscussPostController.java
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -1,10 +1,14 @@
 package com.nowcoder.community.controller;
 
 
+import com.nowcoder.community.entity.Comment;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.CommentService;
 import com.nowcoder.community.service.DiscussPostService;
 import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +19,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Date;
+import java.util.*;
 
 @Controller
 @RequestMapping("/discuss")
-public class DiscussPostController {
+public class DiscussPostController implements CommunityConstant {
     @Autowired
     private DiscussPostService discussPostService;
 
@@ -28,6 +32,9 @@ public class DiscussPostController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private CommentService commentService;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -49,13 +56,67 @@ public class DiscussPostController {
     }
 
     @RequestMapping(value = "/detail/{discussPostId}", method = RequestMethod.GET)
-    public String getDiscussPost(Model model, @PathVariable("discussPostId") int id){
+    // 注意：当一个实体类（java bean）声明在方法的参数中时，springmvc都会把其存入model中
+    public String getDiscussPost(Model model, @PathVariable("discussPostId") int id, Page page){
         // 帖子
         DiscussPost post = discussPostService.findDiscussPostById(id);
         model.addAttribute("post", post);
         // 帖子作者
         User user = userService.findUserById(post.getUserId());
         model.addAttribute("user", user);
+
+        // 评论的分页信息
+        page.setLimit(5);
+        page.setPath("/discuss/detail/"+id);
+        page.setRows(post.getCommentCount());
+
+        // 评论：帖子的评论
+        // 回复：评论的评论
+        // VO：view object，需要被前端模板使用的数据对象
+        // 获取帖子的评论并封装
+        List<Comment> commentList = commentService.findCommentsByEntity(
+                ENTITY_TYPE_POST, post.getId(), page.getOffset(), page.getLimit());
+        // 评论VO列表
+        List<Map<String, Object>> commentVoList = new ArrayList<>();
+        if(commentList != null){
+            for(Comment comment : commentList){
+                // 评论VO
+                Map<String, Object> commentVo = new HashMap<>();
+                // 评论
+                commentVo.put("comment", comment);
+                // 作者
+                commentVo.put("user", userService.findUserById(comment.getUserId()));
+
+                // 回复列表，不对回复做分页处理
+                List<Comment> replyList = commentService.findCommentsByEntity(
+                        ENTITY_TYPE_COMMENT, comment.getId(), 0, Integer.MAX_VALUE);
+                // 回复VO列表
+                List<Map<String, Object>> replyVoList = new ArrayList<>();
+                if(replyList != null){
+                    for(Comment reply : replyList){
+                        Map<String, Object> replyVo = new HashMap<>();
+                        // 回复
+                        replyVo.put("reply", reply);
+                        // 作者
+                        replyVo.put("user", userService.findUserById(reply.getUserId()));
+                        // 回复的目标
+                        User target = reply.getTargetId() == 0 ? null : userService.findUserById(reply.getTargetId());
+                        replyVo.put("target", target);
+
+                        replyVoList.add(replyVo);
+                    }
+                }
+                // 每一个comment的reply
+                commentVo.put("replies", replyVoList);
+
+                // 回复数量
+                int replyCount = commentService.findCommentCount(ENTITY_TYPE_COMMENT, comment.getId());
+                commentVo.put("replyCount", replyCount);
+
+                commentVoList.add(commentVo);
+            }
+        }
+        model.addAttribute("comments", commentVoList);
 
         return "/site/discuss-detail";
     }
---
+++ src/main/java/com/nowcoder/community/dao/CommentMapper.java
@@ -0,0 +1,15 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+    // 需要支持分页的功能
+    List<Comment> selectCommentsByEntity(int entityType, int entityId, int offset, int limit);
+
+    int selectCountByEntity(int entityType, int entityId);
+
+}
