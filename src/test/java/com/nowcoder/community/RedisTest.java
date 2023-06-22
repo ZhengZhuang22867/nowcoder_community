d9c5202d69d31765682d8f95619bd66f1d30d6d9
Zheng Zhuang <1525019749@qq.com>
2023-06-22 14:35:25 +0100

    4.7 spring整合redis 内容：   1.导入依赖；   2.更新application.properties，配置redis的基本信息；   3.创建RedisConfig，完成redis的配置；   4.创建RedisTest，练习redis在idea中的函数操作 以及 redis的事务处理。

---
+++ src/test/java/com/nowcoder/community/RedisTest.java
@@ -0,0 +1,140 @@
+package com.nowcoder.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.*;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = CommunityApplication.class)
+public class RedisTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testStrings() {
+        // 存数据
+        String redisKey = "test:count";
+        redisTemplate.opsForValue().set(redisKey, 1);
+        // 取数据
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        // 增加或减少数据
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+        System.out.println(redisTemplate.opsForValue().decrement(redisKey));
+    }
+
+    @Test
+    public void testHashes() {
+        // 存数据
+        String redisKey = "test:user";
+        redisTemplate.opsForHash().put(redisKey, "id", 1);
+        redisTemplate.opsForHash().put(redisKey, "username", "zhangsan");
+        // 取数据
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "id"));
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "username"));
+    }
+
+    @Test
+    public void testLists() {
+        // 存数据
+        String redisKey = "test:ids";
+        redisTemplate.opsForList().leftPush(redisKey, 101);
+        redisTemplate.opsForList().leftPush(redisKey, 102);
+        redisTemplate.opsForList().rightPush(redisKey, 103);
+        // 获取key的大小；获取某个索引的值；获取某个范围的值
+        System.out.println(redisTemplate.opsForList().size(redisKey));
+        System.out.println(redisTemplate.opsForList().index(redisKey, 0));
+        System.out.println(redisTemplate.opsForList().range(redisKey, 0, 2));
+        // 弹出数据
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+        System.out.println(redisTemplate.opsForList().rightPop(redisKey));
+    }
+
+    @Test
+    public void testSets() {
+        String redisKey = "test:teachers";
+
+        redisTemplate.opsForSet().add(redisKey, "刘备", "关羽", "张飞", "诸葛亮");
+
+        System.out.println(redisTemplate.opsForSet().size(redisKey));
+        System.out.println(redisTemplate.opsForSet().members(redisKey));
+        System.out.println(redisTemplate.opsForSet().pop(redisKey));
+        System.out.println(redisTemplate.opsForSet().pop(redisKey));
+        System.out.println(redisTemplate.opsForSet().members(redisKey));
+    }
+
+    @Test
+    public void testSortedSets() {
+        String redisKey = "test:students";
+
+        redisTemplate.opsForZSet().add(redisKey, "唐僧", 80);
+        redisTemplate.opsForZSet().add(redisKey, "悟空", 90);
+        redisTemplate.opsForZSet().add(redisKey, "八戒", 50);
+        redisTemplate.opsForZSet().add(redisKey, "沙僧", 70);
+        redisTemplate.opsForZSet().add(redisKey, "白龙马", 60);
+
+        System.out.println(redisTemplate.opsForZSet().size(redisKey)); //Zcard()也是一样的效果
+        System.out.println(redisTemplate.opsForZSet().score(redisKey, "八戒"));
+        System.out.println(redisTemplate.opsForZSet().rank(redisKey, "八戒"));
+        System.out.println(redisTemplate.opsForZSet().reverseRank(redisKey, "八戒"));
+        System.out.println(redisTemplate.opsForZSet().range(redisKey, 0,2));
+        System.out.println(redisTemplate.opsForZSet().reverseRange(redisKey, 0,2));
+    }
+
+    @Test
+    public void testKeys(){
+        redisTemplate.delete("test:user");
+
+        System.out.println(redisTemplate.hasKey("test:user"));
+
+        redisTemplate.expire("test:students", 10, TimeUnit.SECONDS);
+    }
+
+    // 当针对同一个key进行一系列的操作时，就可以绑定key对象从而简化代码
+    @Test
+    public void testBoundOperations(){
+        String redisKey = "test:count";
+        BoundValueOperations operations = redisTemplate.boundValueOps(redisKey);
+
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        operations.increment();
+
+        System.out.println(operations.get());
+    }
+
+    // redis也同样支持事务，但是和mysql不同的是，代码中我们使用编程式的方法
+    @Test
+    public void testTransactional(){
+        Object obj = redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String redisKey = "test:tx";
+                // 开启事务，即以下的多个操作要同时提交
+                operations.multi();
+
+                operations.opsForSet().add(redisKey, "zhangsan");
+                operations.opsForSet().add(redisKey, "lisi");
+                operations.opsForSet().add(redisKey, "wangwu");
+
+                // 这是一个无效查询，用于验证在事务提交之前，不会有数据被查到
+                System.out.println(operations.opsForSet().members(redisKey));
+
+                return operations.exec();
+            }
+        });
+        // 这是一个有效查询，证明事务已经提交
+        System.out.println(obj);
+    }
+}
