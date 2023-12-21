e01f6db087712ec58bebe461422626ebfde41bd2
ZhengZhuang22867 <1525019749@qq.com>
2023-12-21 08:23:36 +0800

    在MateBook14s重新部署项目，测试git连通性。

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -11,9 +11,6 @@ public class CommunityApplication {
 		// 这是一个配置类，在这里启动项目。启动后，底层会启动tomcat，同时会创建spring容器，容器会自动扫描bean并将其装入容器中
 		SpringApplication.run(CommunityApplication.class, args);
 
-		// 没有代码变化，只是换了新电脑测试下git是否好用
-
-		// 在终端设置spark的东西，检查下key使否被重制，与内容无关
 	}
 
 }
